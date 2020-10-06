26a475d102fc5e614114ee5ed32088d84e55216d
ahkam-9408 <it18098174@my.sliit.lk>
2020-10-06 11:30:52 +0530

    Make Payment Progress

--- app/src/main/java/com/bojitha/myapplication/MakePayment/SelectBills.java
+++ app/src/main/java/com/bojitha/myapplication/MakePayment/SelectBills.java
@@ -1,32 +1,56 @@
 package com.bojitha.myapplication.MakePayment;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.GridView;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.bojitha.myapplication.R;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import static com.bojitha.myapplication.MakePayment.ItemAdapter.SPAN_COUNT_ONE;
+import static com.bojitha.myapplication.MakePayment.ItemAdapter.SPAN_COUNT_THREE;
 
 public class SelectBills extends AppCompatActivity {
 
-    private ArrayList<String> billersList;
-
+    private RecyclerView recyclerView;
+    private ItemAdapter itemAdapter;
+    private GridLayoutManager gridLayoutManager;
+    private List<Item> items;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_select_bills);
+
+        AppCompatDelegate.setCompatVectorFromResourcesEnabled(true);
+
+        initItemsData();
+
+        gridLayoutManager = new GridLayoutManager(this, SPAN_COUNT_ONE);
+        itemAdapter = new ItemAdapter(items, gridLayoutManager);
+        recyclerView = (RecyclerView) findViewById(R.id.recyView);
+        recyclerView.setAdapter(itemAdapter);
+        recyclerView.setLayoutManager(gridLayoutManager);
     }
 
-    private void loadBillers(View view){
-        GridView billersGrid = (GridView)findViewById(R.id.grid_view);
-        billersList = new ArrayList<>();
-        for (int i=1;i<=20;i++){
-            billersList.add("Biller "+i);
-        }
+    private void initItemsData() {
+        items = new ArrayList<>(4);
+        items.add(new Item(R.drawable.dialog_logo, "My Dialgo Mobile", "0748523245"));
+        items.add(new Item(R.drawable.ceb_logo, "My Home Elec Bill", "100120150160"));
+        items.add(new Item(R.drawable.water_logo, "Water Bill", "8832145897"));
+        items.add(new Item(R.drawable.dialog_logo, "Father Dialog", "0746885522"));
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
     }
 }
